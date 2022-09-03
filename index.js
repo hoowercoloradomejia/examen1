c68221f3f044ab35127ee72839d8536599d20636
hoowercoloradomejia <hoowercoloradomejia@gmail.com>
2022-09-03 10:17:00 -0500

    agrgar base de datos puerto 3000

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express")
-
+const mongoose = require("mongoose")
 
 const app = express()
-const port = 3000
-app.listen(port, ()=>{console.log('conect with the port',port)});
+const port = process.env.PORT||3000
+const uri=`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@electiva1.mtunbw2.mongodb.NET/${process.env.DBNAME}}`
+
+mongoose.connect(uri)
+.then(console.log(`data base connected`))
+.catch((error)=>console.log(error))
+app.listen(port, ()=>{console.log(`Connect with the port`, port)})
